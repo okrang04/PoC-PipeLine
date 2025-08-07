e8f4c0f35d6a06f711c35c860a094f66ead0405f
unknown <okrang04@mz.co.kr>
2025-08-07 18:20:31 +0900

    KTDSPOC-13 연동테스트

--- src/main/java/com/mz/cicd/poc/PocApplication.java
+++ src/main/java/com/mz/cicd/poc/PocApplication.java
@@ -9,7 +9,7 @@ public class PocApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PocApplication.class, args);
 		//Servicenow webhook test
-		System.out.println("KTDSPOC-1 테스트4");
+		System.out.println("KTDSPOC-1 테스트5");
 	}
 
 }

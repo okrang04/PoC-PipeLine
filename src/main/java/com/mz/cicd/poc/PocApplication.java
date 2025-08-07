370baec2682cce39b283fb7e3ec4c90721b8b90b
unknown <okrang04@mz.co.kr>
2025-08-07 17:47:54 +0900

    KTDSPOC-1: commit3

--- src/main/java/com/mz/cicd/poc/PocApplication.java
+++ src/main/java/com/mz/cicd/poc/PocApplication.java
@@ -9,7 +9,7 @@ public class PocApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PocApplication.class, args);
 		//Servicenow webhook test
-		System.out.println("SCRUM-13 테스트3");
+		System.out.println("KTDSPOC-1 테스트4");
 	}
 
 }
